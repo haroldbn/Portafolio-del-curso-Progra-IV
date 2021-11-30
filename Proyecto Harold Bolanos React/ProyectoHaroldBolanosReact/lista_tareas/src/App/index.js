b19c4131084c7ce05d0fffc195600f11ddd63dc7
haroldbn <haroldbolanosn@gmail.com>
2021-11-29 22:46:11 -0600

    proyecto final harold bolanos

---
+++ Proyecto Harold Bolanos React/ProyectoHaroldBolanosReact/lista_tareas/src/App/index.js
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react'
+import { UI } from './ui'
+import { AppProvider } from '../Context'
+
+
+function App() {
+  return (
+    <AppProvider>
+      <UI />
+    </AppProvider>
+  )
+}
+
+export default App;
--- Proyecto Harold Bolanos React/ProyectoHaroldBolanosReact/lista_tareas/src/BarraBusqueda/index.js
+++ Proyecto Harold Bolanos React/ProyectoHaroldBolanosReact/lista_tareas/src/BarraBusqueda/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import './BarraBusqueda.css'
-function BarraBusqueda({valorBusqueda, funcionBuscar}){
+import { AppContext } from '../Context/'
+
+function BarraBusqueda(){
+  const {valorBuscado, buscarTarea} = React.useContext(AppContext)
   const buscar=(event)=>{
-    funcionBuscar(event.target.value)
+    buscarTarea(event.target.value)
   }
 return (
   <input
   className="BarraBusqueda"
   placeholder="Tarea a buscar"
   onChange={buscar}
-  value={valorBusqueda}
+  value={valorBuscado}
   />
 )
 }
